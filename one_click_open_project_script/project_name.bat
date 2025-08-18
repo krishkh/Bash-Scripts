9b84ec633e3830657cf6a7e9a50aad6c0e7ae132
krish khanna <51072063+krishkh@users.noreply.github.com>
2025-08-18 15:11:42 +0530

    Update project_name.bat: Now the terminals run in background
    extra terminals wont popup when clicked upon the bat file

--- one_click_open_project_script/project_name.bat
+++ one_click_open_project_script/project_name.bat
@@ -1,10 +1,10 @@
 @echo off
 
 REM Open in Cursor
-start "" cursor "Drive:\pathA\pathB\pathC\project_name"
+start "" /B cursor "Drive:\pathA\pathB\pathC\project_name"
 
 REM Wait a few seconds (give the server time to start)
 timeout /t 3 >nul
 
 REM Open browser like below example
-start "" "http://localhost:3000"
+start "" /B "http://localhost:3000"
